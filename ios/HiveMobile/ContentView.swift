3ae2967465787051946965b68a6c1963511b4f2b
0xlny <lenny@419labs.io>
2026-02-17 22:27:12 +0100

    init iOS SwiftUI project (iOS 26, Liquid Glass ready)

---
+++ ios/HiveMobile/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hive")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/HiveMobile/HiveApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct HiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
